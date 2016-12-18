6c18e261ef1fbdeda95832911d41e97d0e95a35f
Gabriel Joshua Paet <gjvpaet@gmail.com>
2016-12-18 14:31:13 +0800

    Added authentication scaffolding

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Rio\'s Library',
 
     /*
     |--------------------------------------------------------------------------
@@ -172,11 +172,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        Library\Providers\AppServiceProvider::class,
+        Library\Providers\AuthServiceProvider::class,
+        // Library\Providers\BroadcastServiceProvider::class,
+        Library\Providers\EventServiceProvider::class,
+        Library\Providers\RouteServiceProvider::class,
 
     ],
 
